9c078f0e9961dbf7364a1ac7b59c574dc7e7c315
Evaristo Alves <evaristocarvalho34@gmail.com>
2021-11-09 19:05:45 -0300

    criado repositorio

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿
+using Enquete.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Enquete.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Responder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Responder(Resposta resposta)
+        {
+            if (ModelState.IsValid)
+            {
+                Repositorio.AdicionarResposta(resposta);
+                return View("Obrigado");
+            }
+            else
+            {
+                return View(resposta);
+            }
+            
+        }
+
+        public IActionResult Resultado()
+        {
+            return View(Repositorio.Respostas);
+        }
+
+    }
+}
